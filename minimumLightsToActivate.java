750d13232dbf2ad41870247f218d7454d7cd1ec3
Rishav Garg <rishavag.edu@gmail.com>
2022-02-25 20:05:00 +0530

    improvedMinimumLightsToActivate

--- minimumLightsToActivate.java
+++ minimumLightsToActivate.java
@@ -1,35 +1,25 @@
 public class Solution {
-    public int solve(ArrayList<Integer> A, int B) {
-        if(A.size()<B){
-            // if there are less number of elements in A than B, then lighting any one of the lights will give us an answer.
-            if(A.indexOf(1)!=-1) return 1;
-            else return -1;
-        }
-        // minDone and maxDone will store the values of the light that was switched on before and the last index where we can see the effect respectively
-        int minDone = 0;
-        int maxDone = 0;
-        // this will traverse and handle the indexes in the array
-        int curLight = B-1;
-        // this will be our final answer
+    public int solve(ArrayList < Integer > A, int B) {
+        int i = 0;
         int count = 0;
-        // while the last light has not been switched on
-        while(maxDone<A.size()-1){
-            // initially, our curLight is set to the most rightmost possible light that can light up all possible indexes before it. Hence, we come back to ensure we get the rightmost functional light.
-            while(A.get(curLight) == 0){
-                // if we reach the first light, or the light which was switched on earlier without encountering even a single 1, then it is impossible to light the particular indexes
-                if(curLight == 0 || curLight == minDone+1) return -1;
-                curLight--;
+        int n = A.size();
+        while (i < n) {
+            int right = Math.min(i + B - 1, n - 1);
+            int left = Math.max(i - B + 1, 0);
+            boolean bulbFound = false;
+            while (right >= left) {
+                if (A.get(right) == 1) {
+                    bulbFound = true;
+                    break;
+
+                }
+                right--;
             }
-            // one more light to be turned on
+            if (!bulbFound) {
+                return -1;
+            }
             count++;
-            // we set the last light switched on to curLight
-            minDone = curLight;
-            // this is the index of the last index that could be affected due to the light swithed on
-            maxDone = curLight+B-1;
-            // we set curLight to maxDone+B, to ensure if we turn it on, all lights in between can be switched on too.
-            curLight = Math.min(A.size()-1,maxDone+B);
+            i = right + B;
         }
-        // we return the count;
         return count;
-    }
-}
+    }}
